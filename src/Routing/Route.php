e6618131d4c91ff6033e536c6d12bd185e9c10b4
David Grudl <david@grudl.com>
2015-01-18 14:05:38 +0100

    Route: speed optimization for static masks

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -454,6 +454,13 @@ class Route extends Nette\Object implements Application\IRouter
 			}
 		}
 
+		if (strpbrk($mask, '?<[') === FALSE) {
+			$this->re = '#' . preg_quote($mask, '#') . '/?\z#A';
+			$this->sequence = array($mask);
+			$this->metadata = $metadata;
+			return;
+		}
+
 		// PARSE MASK
 		// <parameter-name[=default] [pattern]> or [ or ] or ?...
 		$parts = Strings::split($mask, '/<([^>= ]+)(=[^> ]*)? *([^>]*)>|(\[!?|\]|\s*\?.*)/');
