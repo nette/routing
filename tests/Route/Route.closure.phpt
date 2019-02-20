nette/application

tests: added test

tests/Routers/Route.closure.phpt
@@ -1,0 +1,23 @@
+<?php
+
+/**
+ * Test: Nette\Application\Routers\Route with closure.
+ */
+
+use Nette\Application\Routers\Route;
+use Tester\Assert;
+
+
+require __DIR__ . '/../bootstrap.php';
+
+require __DIR__ . '/Route.inc';
+
+
+$closure = function() {};
+$route = new Route('<id>', $closure);
+
+testRouteIn($route, '/12', 'Nette:Micro', [
+	'id' => '12',
+	'test' => 'testvalue',
+	'callback' => $closure,
+], '/12?test=testvalue');

nette/routing

tests/Route/Route.closure.phpt
@@ -1,0 +1,23 @@
+<?php
+
+/**
+ * Test: Nette\Application\Routers\Route with closure.
+ */
+
+use Nette\Application\Routers\Route;
+use Tester\Assert;
+
+
+require __DIR__ . '/../bootstrap.php';
+
+require __DIR__ . '/Route.php';
+
+
+$closure = function() {};
+$route = new Route('<id>', $closure);
+
+testRouteIn($route, '/12', 'Nette:Micro', [
+	'id' => '12',
+	'test' => 'testvalue',
+	'callback' => $closure,
+], '/12?test=testvalue');