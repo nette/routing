091536c9abdddcb1f81965cfec39f5e56c5580a2
milos.brecher <milos.brecher@gmail.com>
2024-12-02 06:15:44 +0100

    correction of bug made by AI :)

--- src/Routing/Route.php
+++ src/Routing/Route.php
@@ -163,7 +163,7 @@ class Route implements Router
 		if ($this->type === self::Host) {
 			$host = $url->getHost();
 			$path = '//' . $host . $url->getPath();
-            $parts = filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)
+            $parts = filter_var($host, FILTER_VALIDATE_IP)
                 ? [$host]
                 : array_reverse(explode('.', $host));
 			$re = strtr($re, [
@@ -269,7 +269,7 @@ class Route implements Router
 
 		} else {
 			$host = $refUrl->getHost();
-            $parts = filter_var($host, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)
+            $parts = filter_var($host, FILTER_VALIDATE_IP)
                 ? [$host]
                 : array_reverse(explode('.', $host));
             $port = $refUrl->getDefaultPort() === ($tmp = $refUrl->getPort()) ? '' : ':' . $tmp;
